a4b00cfafe2e8f95ab07b934dcd1182ca5568986
venkat <venkata.ch@assiduusinc.com>
2025-07-15 14:13:06 +0530

    #updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/js-catering-delights-indian/' : '/',
+  base: '/js-catering-delights-indian/',
   server: {
     host: "::",
     port: 8080,
